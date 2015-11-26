dbaddb32877b9de5860796aab224f7839a3583c7
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-11-26 13:55:57 -0500

    Add code to convert GBSAOBCForce to CustomGBForce

--- src/unfolded-model.py
+++ src/unfolded-model.py
@@ -26,7 +26,7 @@ version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -70,10 +70,47 @@ def _findForces(system, force_name, first_only=False):
 
     return forces
 
+def replaceGBSAOBCForce(system):
+    """
+    If the System contains a GBSAOBCForce, replace it with a CustomGBForce.
+
+    """
+    from simtk.openmm.app.internal.customgbforces import GBSAOBC2Force
+
+    # Go through forces in reverse so we can delete GB forces as we go along.
+    for force_index in range(system.getNumForces()-1, 0, -1):
+        force = system.getForce(force_index)
+        if (force.__class__.__name__ != 'GBSAOBCForce'):
+            continue
+
+        # Convert GBSAOBCForce to CustomGBForce
+        cutoff = None
+        if force.getNonbondedMethod() == 'CutoffNonPeriodic':
+            cutoff = force.getCutoffDistance().value_in_unit_system(unit.md_unit_system)
+
+        custom_force = GBSAOBC2Force(
+            solventDielectric=force.getSolventDielectric(),
+            soluteDielectric=force.getSoluteDielectric(),
+            cutoff=cutoff,
+            SA='ACE')
+
+        custom_force.setNonbondedMethod(force.getNonbondedMethod()) # TODO: Go through enum types rather than integers
+        custom_force.setCutoffDistance(force.getCutoffDistance())
+
+        for particle_index in range(force.getNumParticles()):
+            [charge, radius, scalingFactor] = force.getParticleParameters(particle_index)
+            custom_force.addParticle([charge, radius, scalingFactor])
+
+        # Add new force and delete old force.
+        system.addForce(custom_force)
+        system.removeForce(force_index)
+
+    return
+
 def createConstruct(topology, reference_system, construct_residues):
     """
     Create a construct where interactions outside of specified construct residues are set to noninteracting 'ghost' particles.
-    
+
     Parameters
     ----------
     topology : simtk.openmm.app.Topology
@@ -97,7 +134,7 @@ def createConstruct(topology, reference_system, construct_residues):
     # Get NonbondedForce objects for system copy.
     nonbonded_force = _findForces(system, 'NonbondedForce', first_only=True)
     gb_force = _findForces(system, 'GBSAOBCForce', first_only=True)
-    
+
     # Turn off charges and Lennard-Jones parameters for atoms outside of the construct.
     zero_charge = 0.0 * unit.elementary_charge
     unit_sigma = 1.0 * unit.angstroms
@@ -143,6 +180,7 @@ def createUnfoldedSurrogate(topology, reference_system, locality=5):
     TODO
     ----
     * Replace NonbondedForce with CustomBondForce for improved efficiency.
+    * Replace CustomGBForce with CustomBondForce for improved efficiency.
 
     """
 
@@ -150,31 +188,36 @@ def createUnfoldedSurrogate(topology, reference_system, locality=5):
     system = copy.deepcopy(reference_system)
 
     # Get NonbondedForce objects for system copy.
-    force = _findForces(system, 'NonbondedForce', first_only=True)
+    nonbonded_force = _findForces(system, 'NonbondedForce', first_only=True)
+    gb_force = _findForces(system, 'CustomGBForce', first_only=True)
 
     # Add exclusions for non-local interactions.
     for atom1 in topology.atoms():
-        [charge1, sigma1, epsilon1] = force.getParticleParameters(atom1.index)
+        [charge1, sigma1, epsilon1] = nonbonded_force.getParticleParameters(atom1.index)
         for atom2 in topology.atoms():
-            if (atom1.index < atom2.index) and (abs(atom1.residue.index - atom2.residue.index) <= locality):
-                # Create exclusion.
-                try:
-                    [charge2, sigma2, epsilon2] = force.getParticleParameters(atom1.index)
-                    chargeprod = charge1 * charge2
-                    sigma = 0.5 * (sigma1 + sigma2)
-                    epsilon = unit.sqrt(epsilon1 * epsilon2)
-                    force.addException(atom1.index, atom2.index, chargeprod, sigma, epsilon, False)
-                except:
-                    # Exception already exists; don't modify it.
-                    pass
+            if (atom1.index < atom2.index):
+                if (abs(atom1.residue.index - atom2.residue.index) <= locality):
+                    # Create exclusion.
+                    try:
+                        [charge2, sigma2, epsilon2] = nonbonded_force.getParticleParameters(atom1.index)
+                        chargeprod = charge1 * charge2
+                        sigma = 0.5 * (sigma1 + sigma2)
+                        epsilon = unit.sqrt(epsilon1 * epsilon2)
+                        nonbonded_force.addException(atom1.index, atom2.index, chargeprod, sigma, epsilon, False)
+                    except:
+                        # Exception already exists; don't modify it.
+                        pass
+                else:
+                    # Exclude GB interactions between nonlocal particles.
+                    gb_force.addExclusion(atom1.index, atom2.index)
 
     # Turn off standard particle interactions.
     zero_charge = 0.0 * unit.elementary_charge
     unit_sigma = 1.0 * unit.angstroms
     zero_epsilon = 0.0 * unit.kilocalories_per_mole
     for atom in topology.atoms():
-        force.setParticleParameters(atom.index, zero_charge, unit_sigma, zero_epsilon)
-    
+        nonbonded_force.setParticleParameters(atom.index, zero_charge, unit_sigma, zero_epsilon)
+
     # Return modified system.
     return system
 
@@ -182,11 +225,11 @@ def createUnfoldedSurrogate2(topology, reference_system, locality=5):
 
     # Create new deep copy reference system to modify.
     system = openmm.System()
-    
+
     # Set periodic box vectors.
     [a,b,c] = reference_system.getDefaultPeriodicBoxVectors()
     system.setDefaultPeriodicBoxVectors(a,b,c)
-        
+
     # Add atoms.
     for atom_index in range(reference_system.getNumParticles()):
         mass = reference_system.getParticleMass(atom_index)
@@ -195,7 +238,7 @@ def createUnfoldedSurrogate2(topology, reference_system, locality=5):
     # Add constraints
     for constraint_index in range(reference_system.getNumConstraints()):
         [iatom, jatom, r0] = reference_system.getConstraintParameters(constraint_index)
-        system.addConstraint(iatom, jatom, r0)    
+        system.addConstraint(iatom, jatom, r0)
 
     # Modify forces as appropriate, copying other forces without modification.
     nforces = reference_system.getNumForces()
@@ -205,20 +248,20 @@ def createUnfoldedSurrogate2(topology, reference_system, locality=5):
         if isinstance(reference_force, openmm.NonbondedForce):
             # Create CustomBondForce instead.
             energy_expression = "4*epsilon*((sigma/r)^12 - (sigma/r)^6) + 138.935456*chargeprod/r;" # TODO: Check coulomb constant
-            custom_bond_force = openmm.CustomBondForce(energy_expression)            
+            custom_bond_force = openmm.CustomBondForce(energy_expression)
             custom_bond_force.addPerBondParameter("chargeprod") # charge product
             custom_bond_force.addPerBondParameter("sigma") # Lennard-Jones sigma
             custom_bond_force.addPerBondParameter("epsilon") # Lennard-Jones epsilon
             #system.addForce(custom_bond_force)
-            
+
             # Add exclusions.
             from sets import Set
             exceptions = Set()
             for index in range(reference_force.getNumExceptions()):
                 [atom1_index, atom2_index, chargeprod, sigma, epsilon] = reference_force.getExceptionParameters(index)
                 custom_bond_force.addBond(atom1_index, atom2_index, [chargeprod, sigma, epsilon])
-                
-                if atom2_index < atom1_index: 
+
+                if atom2_index < atom1_index:
                     exceptions.add( (atom2_index, atom1_index) )
                 else:
                     exceptions.add( (atom1_index, atom2_index) )
@@ -238,17 +281,61 @@ def createUnfoldedSurrogate2(topology, reference_system, locality=5):
             # Copy force without modification.
             force = copy.deepcopy(reference_force)
             #system.addForce(force)
-            
+
             zero_charge = 0.0 * unit.elementary_charge
             unit_sigma = 1.0 * unit.angstroms
-            for atom in topology.atoms():            
-                force.setParticleParameters(atom.index, zero_charge, unit_sigma, 0.0)                
-        else:                
-            
+            for atom in topology.atoms():
+                force.setParticleParameters(atom.index, zero_charge, unit_sigma, 0.0)
+        else:
+
             # Copy force without modification.
             force = copy.deepcopy(reference_force)
             system.addForce(force)
 
+    return system
+
+def createUnfoldedSurrogateCutoff(topology, reference_system, cutoff=5.0*unit.angstrom, switch_width=1.0*unit.angstrom):
+    """
+    Create a surrogate for the unfolded state in which non-local residue interactions are eliminated
+    by using a short cutoff.
+
+    Parameters
+    ----------
+    topology : simtk.openmm.app.Topology
+        The Topology object for the reference system.
+    reference_system : simtk.openmm.System
+        The System object for the reference system to be modified.
+    cutoff : simtk.unit.Quantity with dimension distance, optional, default=5*angstrom
+        The cutoff to use for nonbonded interactions.
+    switch_width : simtk.unit.Quantity with dimension distance, optional, default=1*angstrom
+        The switch width to use.
+
+    Returns
+    -------
+    system : simtk.openmm.System
+        A System object corresponding to the desired construct.
+
+    NOTE: This is only appropriate for GBSA systems.
+
+    """
+
+    # Make a copy of the reference system if a working copy is not specified.
+    system = copy.deepcopy(reference_system)
+
+    # Set cutoff for all force objects.
+    for force_index in range(system.getNumForces()):
+        force = system.getForce(force_index)
+
+        if hasattr(force, 'setNonbondedMethod'):
+            force.setNonbondedMethod(getattr(force, 'CutoffNonPeriodic'))
+        if hasattr(force, 'setCutoffDistance'):
+            force.setCutoffDistance(cutoff)
+        if hasattr(force, 'setUseSwitchingFunction'):
+            force.setUseSwitchingFunction(True)
+        if hasattr(force, 'setSwitchingDistance'):
+            force.setSwitchingDistance(cutoff - switch_width)
+
+    # Return modified system.
     return system
 
 def benchmark(system, positions, nsteps=500):
@@ -260,18 +347,18 @@ def benchmark(system, positions, nsteps=500):
     context.setPositions(positions)
     state = context.getState(getEnergy=True)
     print state.getPotentialEnergy()
-    print "Minimizing..."
+    print("    Minimizing...")
     tolerance = 1.0 * unit.kilocalories_per_mole / unit.angstrom
     maxIterations = 500
     openmm.LocalEnergyMinimizer.minimize(context, tolerance, maxIterations)
-    print "Simulating..."
+    print("    Simulating...")
     initial_time = time.time()
     integrator.step(nsteps)
     state = context.getState(getEnergy=True)
     final_time = time.time()
     elapsed_time = final_time - initial_time
     print state.getPotentialEnergy()
-    print "%d steps took %.3f s (%.3f ns/day)" % (nsteps, elapsed_time, nsteps * (timestep/unit.nanoseconds) / elapsed_time * 24*60*60)
+    print("%d steps took %.3f s (%.3f ns/day)" % (nsteps, elapsed_time, nsteps * (timestep/unit.nanoseconds) / elapsed_time * 24*60*60))
     del context, integrator, state
     return
 
@@ -296,13 +383,17 @@ def simulate(topology, system, positions, output_filename):
     state = simulation.context.getState(getPositions=True)
     reporter.report(simulation, state)
 
+    print("    Minimizing...")
     simulation.minimizeEnergy(maxIterations=50)
 
     # Write first frame.
     state = simulation.context.getState(getPositions=True)
     reporter.report(simulation, state)
 
-    simulation.step(niterations * nsteps)
+    print("    Simulating...")
+    for iteration in range(niterations):
+        print("        Iteration %d / %d" % (iteration, niterations))
+        simulation.step(nsteps)
 
     return
 
@@ -323,14 +414,17 @@ if __name__ == "__main__":
     forcefield = app.ForceField('amber96.xml', 'amber96_obc.xml')
     print "Parameterizing protein..."
     reference_system = forcefield.createSystem(pdb.topology, constraints=True)
+    print "Replacing GBSAOBCForce with CustomGBForce to allow exclusions to be added..."
+    replaceGBSAOBCForce(reference_system)
 
-    print "Testing unfolded mimic..."
+    print "Creating unfolded surrogate..."
     system = createUnfoldedSurrogate(pdb.topology, reference_system, locality=5)
+    #system = createUnfoldedSurrogateCutoff(pdb.topology, system)
 
-    #print "Benchmarking unfolded mimic..."
+    #print "Benchmarking unfolded surrogate..."
     #benchmark(system, pdb.positions)
-    
-    print "Simulating unfolded mimic..."
+
+    print "Simulating unfolded surrogate..."
     simulate(pdb.topology, system, pdb.positions, 'unfolded.pdb')
 
     print "Testing construct creation..."
@@ -340,7 +434,6 @@ if __name__ == "__main__":
     construct_residues = range(first_residue + 5, last_residue - 5 + 1)
     print construct_residues
     system = createConstruct(pdb.topology, reference_system, construct_residues)
-    
+
     print "Simulating construct..."
     benchmark(system, pdb.positions)
-
